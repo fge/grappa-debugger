8a4e67842ce279543608b2d8afa57e52093635ba
Francis Galiegue <fgaliegue@gmail.com>
2015-01-10 22:04:57 +0100

    More code...

--- src/main/java/com/github/parboiled1/grappa/debugger/presenter/GrappaDebuggerPresenter.java
+++ src/main/java/com/github/parboiled1/grappa/debugger/presenter/GrappaDebuggerPresenter.java
@@ -2,6 +2,10 @@ package com.github.parboiled1.grappa.debugger.presenter;
 
 import com.github.parboiled1.grappa.debugger.view.GrappaDebuggerView;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Objects;
+
+@ParametersAreNonnullByDefault
 public final class GrappaDebuggerPresenter
 {
     private GrappaDebuggerView view;
@@ -12,13 +16,18 @@ public final class GrappaDebuggerPresenter
 
     public GrappaDebuggerPresenter(final GrappaDebuggerView view)
     {
-        this.view = view;
+        this.view = Objects.requireNonNull(view);
+        initializeView();
     }
 
     // TODO! Find a way NOT to have that
     public void setView(final GrappaDebuggerView view)
     {
-        this.view = view;
+        if (this.view != null)
+            throw new IllegalStateException();
+
+        this.view = Objects.requireNonNull(view);
+        initializeView();
     }
 
     public void display()
@@ -29,5 +38,10 @@ public final class GrappaDebuggerPresenter
     public void close()
     {
         view.close();
+    }
+
+    private void initializeView()
+    {
+        view.setButtonAction(event -> System.out.println("meh"));
     }
 }
--- src/main/java/com/github/parboiled1/grappa/debugger/view/JavafxGrappaDebuggerView.java
+++ src/main/java/com/github/parboiled1/grappa/debugger/view/JavafxGrappaDebuggerView.java
@@ -1,19 +1,35 @@
 package com.github.parboiled1.grappa.debugger.view;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Objects;
+
+@ParametersAreNonnullByDefault
 public final class JavafxGrappaDebuggerView
     implements GrappaDebuggerView
 {
     private final Stage stage;
     private final StackPane root;
+    private final Button button;
 
     public JavafxGrappaDebuggerView(final Stage stage)
     {
         this.stage = stage;
+
         root = new StackPane();
+
+        final ObservableList<Node> children = root.getChildren();
+
+        button = new Button("clickme");
+        children.add(button);
 
         final Scene scene = new Scene(root, 300, 200);
 
@@ -26,6 +42,13 @@ public final class JavafxGrappaDebuggerView
     public void display()
     {
         stage.show();
+    }
+
+    @Override
+    public void setButtonAction(final EventHandler<ActionEvent> handler)
+    {
+        Objects.requireNonNull(handler);
+        button.setOnAction(handler);
     }
 
     @Override
