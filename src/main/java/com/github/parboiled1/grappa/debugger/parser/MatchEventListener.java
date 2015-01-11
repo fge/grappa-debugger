11140c7db086686932df03cf0a0c4f602db79658
Francis Galiegue <fgaliegue@gmail.com>
2015-01-11 05:00:58 +0100

    Modify TestParser and MatchEventListener

--- src/main/java/com/github/parboiled1/grappa/debugger/parser/MatchEventListener.java
+++ src/main/java/com/github/parboiled1/grappa/debugger/parser/MatchEventListener.java
@@ -1,13 +1,21 @@
 package com.github.parboiled1.grappa.debugger.parser;
 
 import com.github.parboiled1.grappa.debugger.mainwindow.MainWindowPresenter;
-import com.github.parboiled1.grappa.run.PreMatchEvent;
+import com.github.parboiled1.grappa.run.MatchFailureEvent;
+import com.github.parboiled1.grappa.run.MatchSuccessEvent;
+import com.google.common.base.Strings;
 import com.google.common.eventbus.Subscribe;
 import org.parboiled.MatcherContext;
+import org.parboiled.support.Position;
 
 public final class MatchEventListener
 {
     private final MainWindowPresenter presenter;
+
+    private int level;
+    private String label;
+    private int lineNumber;
+    private int columnNumber;
 
     public MatchEventListener(final MainWindowPresenter presenter)
     {
@@ -15,13 +23,38 @@ public final class MatchEventListener
     }
 
     @Subscribe
-    public void preMatch(final PreMatchEvent<?> event)
+    public void failure(final MatchFailureEvent<?> event)
     {
-        final StringBuilder sb = new StringBuilder("TRYING: ");
         final MatcherContext<?> context = event.getContext();
-        sb.append(context.getPath())
-            .append(' ').append(context.getPosition())
-            .append('\n');
+        final Position position = context.getPosition();
+        level = context.getLevel();
+        label = context.getMatcher().toString();
+        lineNumber = position.getLine();
+        columnNumber = position.getColumn();
+
+        final StringBuilder sb = new StringBuilder("KO: ");
+        sb.append(Strings.repeat("  ", level)).append(label)
+            .append(" (line ").append(lineNumber)
+            .append(", column ").append(columnNumber)
+            .append(")\n");
+        presenter.addTrace(sb.toString());
+    }
+
+    @Subscribe
+    public void success(final MatchSuccessEvent<?> event)
+    {
+        final MatcherContext<?> context = event.getContext();
+        final Position position = context.getPosition();
+        level = context.getLevel();
+        label = context.getMatcher().toString();
+        lineNumber = position.getLine();
+        columnNumber = position.getColumn();
+
+        final StringBuilder sb = new StringBuilder("OK: ");
+        sb.append(Strings.repeat("  ", level)).append(label)
+            .append(" (line ").append(lineNumber)
+            .append(", column ").append(columnNumber)
+            .append(")\n");
         presenter.addTrace(sb.toString());
     }
 }
--- src/main/java/com/github/parboiled1/grappa/debugger/parser/TestParser.java
+++ src/main/java/com/github/parboiled1/grappa/debugger/parser/TestParser.java
@@ -2,25 +2,30 @@ package com.github.parboiled1.grappa.debugger.parser;
 
 import org.parboiled.BaseParser;
 import org.parboiled.Rule;
+import org.parboiled.annotations.Label;
 
 public class TestParser
     extends BaseParser<String>
 {
+    @Label("unquoted")
     public Rule unquoted()
     {
         return oneOrMore(noneOf("\\\""));
     }
 
+    @Label("quoted")
     public Rule quoted()
     {
         return sequence('\\', ANY);
     }
 
+    @Label("content")
     public Rule content()
     {
         return join(unquoted()).using(quoted()).min(0);
     }
 
+    @Label("quotedString")
     public Rule quotedString()
     {
         return sequence('"', content(), '"', EOI);
