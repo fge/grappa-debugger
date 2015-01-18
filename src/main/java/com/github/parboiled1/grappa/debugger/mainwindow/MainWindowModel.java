ba4cab7522bc194f968dff4c74ff1267fae50f94
Francis Galiegue <fgaliegue@gmail.com>
2015-01-18 15:09:13 +0100

    Remove old files

--- src/main/java/com/github/parboiled1/grappa/debugger/mainwindow/MainWindowModel.java
+++
@@ -1,9 +0,0 @@
-package com.github.parboiled1.grappa.debugger.mainwindow;
-
-import com.github.parboiled1.grappa.debugger.parser.MatchResult;
-import javafx.scene.control.TreeItem;
-
-public interface MainWindowModel
-{
-    TreeItem<MatchResult> runTrace(String inputText);
-}
