89960d9cc1ff9a7f5ddaaa87715eb26b5d0aa353
Francis Galiegue <fgaliegue@gmail.com>
2015-01-30 01:08:30 +0100

    Remove useless files

--- src/main/java/com/github/fge/grappa/debugger/stats/csv/TraceEventLoader.java
+++
@@ -1,8 +0,0 @@
-package com.github.fge.grappa.debugger.stats.csv;
-
-import com.github.fge.grappa.trace.TraceEvent;
-
-public interface TraceEventLoader
-    extends Iterable<TraceEvent>, AutoCloseable
-{
-}
