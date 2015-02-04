0870d4d0f339e85bda6b03ff6ca24fe51ce62196
Francis Galiegue <fgaliegue@gmail.com>
2015-02-04 14:37:47 +0100

    Minore code fixes

--- src/main/java/com/github/fge/grappa/debugger/javafx/parsetree/ParseTreeNodeCell.java
+++ src/main/java/com/github/fge/grappa/debugger/javafx/parsetree/ParseTreeNodeCell.java
@@ -6,7 +6,6 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
-import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
@@ -80,59 +79,13 @@ public final class ParseTreeNodeCell
         treeItemProperty().addListener(listener);
     }
 
-    /**
-     * The updateItem method should not be called by developers, but it is the
-     * best method for developers to override to allow for them to customise the
-     * visuals of the cell. To clarify, developers should never call this method
-     * in their code (they should leave it up to the UI control, such as the
-     * {@link ListView} control) to call this method. However,
-     * the purpose of having the updateItem method is so that developers, when
-     * specifying custom cell factories (again, like the ListView
-     * {@link ListView#cellFactoryProperty() cell factory}),
-     * the updateItem method can be overridden to allow for complete
-     * customisation
-     * of the cell.
-     * <p>It is <strong>very important</strong> that subclasses
-     * of Cell override the updateItem method properly, as failure to do so will
-     * lead to issues such as blank cells or cells with unexpected content
-     * appearing within them. Here is an example of how to properly override the
-     * updateItem method:
-     * <pre>
-     * protected void updateItem(T item, boolean empty) {
-     *     super.updateItem(item, empty);
-     *     if (empty || item == null) {
-     *         setText(null);
-     *         setGraphic(null);
-     *     } else {
-     *         setText(item.toString());
-     *     }
-     * }
-     * </pre>
-     * <p>Note in this code sample two important points:
-     * <ol>
-     * <li>We call the super.updateItem(T, boolean) method. If this is not
-     * done, the item and empty properties are not correctly set, and you are
-     * likely to end up with graphical issues.</li>
-     * <li>We test for the <code>empty</code> condition, and if true, we
-     * set the text and graphic properties to null. If we do not do this,
-     * it is almost guaranteed that end users will see graphical artifacts
-     * in cells unexpectedly.</li>
-     * </ol>
-     *
-     * @param item The new item for the cell.
-     * @param empty whether or not this cell represents data from the list.
-     * If it
-     * is empty, then it does not represent any domain data, but is a cell
-     * being used to render an "empty" row.
-     */
     @Override
     protected void updateItem(final ParseTreeNode item, final boolean empty)
     {
         super.updateItem(item, empty);
-        if (empty || item == null) {
-            setText(null);
+        setText(null);
+        if (empty || item == null)
             setGraphic(null);
-        }
     }
 }
 
