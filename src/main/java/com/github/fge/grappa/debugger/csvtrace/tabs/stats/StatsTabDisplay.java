7b32ae24e6cf3e74d50fcf4cfb522a8fc3cd44fe
Francis Galiegue <fgaliegue@gmail.com>
2015-02-05 11:40:23 +0100

    Some more code modifications

--- src/main/java/com/github/fge/grappa/debugger/csvtrace/tabs/stats/StatsTabDisplay.java
+++ src/main/java/com/github/fge/grappa/debugger/csvtrace/tabs/stats/StatsTabDisplay.java
@@ -4,7 +4,6 @@ import com.github.fge.grappa.debugger.common.JavafxDisplay;
 import com.github.fge.grappa.debugger.common.db.RuleInvocationStatistics;
 import com.github.fge.grappa.debugger.javafx.CallGraphTableCell;
 import com.github.fge.grappa.matchers.MatcherType;
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
@@ -12,6 +11,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+
+import java.util.function.Function;
 
 import static com.github.fge.grappa.debugger.javafx.JavafxUtils.setColumnValue;
 
@@ -82,19 +83,9 @@ public class StatsTabDisplay
         setColumnValue(ruleType, r -> r.getRuleInfo().getType());
         setColumnValue(nrCalls, r -> r.getEmptyMatches() + r.getFailedMatches()
             + r.getNonEmptyMatches());
-        //invocationStatsTable.getSortOrder().add(nrCalls);
         setColumnValue(callDetail, r -> String.format("%d / %d / %d",
             r.getNonEmptyMatches(), r.getEmptyMatches(), r.getFailedMatches()));
-        callGraph.setCellValueFactory(param ->
-            new SimpleObjectProperty<RuleInvocationStatistics>()
-            {
-                @Override
-                public RuleInvocationStatistics get()
-                {
-                    return param.getValue();
-                }
-            }
-        );
+        setColumnValue(callGraph, Function.identity());
         callGraph.setCellFactory(CallGraphTableCell::new);
     }
 
