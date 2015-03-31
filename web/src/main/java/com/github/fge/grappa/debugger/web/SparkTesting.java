de1f7b029c7ed89e39c88e70e6b82bc5e6a10427
Francis Galiegue <fgaliegue@gmail.com>
2015-03-31 17:40:09 +0200

    More modifications; still not working

--- web/src/main/java/com/github/fge/grappa/debugger/web/SparkTesting.java
+++ web/src/main/java/com/github/fge/grappa/debugger/web/SparkTesting.java
@@ -2,11 +2,6 @@ package com.github.fge.grappa.debugger.web;
 
 import spark.Spark;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 public final class SparkTesting
 {
     public static void main(final String... args)
@@ -16,18 +11,7 @@ public final class SparkTesting
         Spark.port(8080);
 
         Spark.post("/upload", (request, response) -> {
-            final Path tempFile = Files.createTempFile("foo", ".bar");
-            try (
-                final InputStream in = request.raw().getInputStream();
-                final OutputStream out = Files.newOutputStream(tempFile);
-            ) {
-                final byte[] buf = new byte[16384];
-
-                int nrBytes;
-                while ((nrBytes = in.read(buf)) != -1)
-                    out.write(buf, 0, nrBytes);
-            }
-            System.out.println("file saved as " + tempFile);
+            System.out.println(request.queryParams());
             response.redirect("/");
             return "OK";
         });
