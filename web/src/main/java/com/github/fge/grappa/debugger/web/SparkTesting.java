2c742640c3675558e7c672064906c78409c5d381
Francis Galiegue <fgaliegue@gmail.com>
2015-03-31 17:35:45 +0200

    Try, and fail, to upload a file...
    The submitted form is empty :(

--- web/src/main/java/com/github/fge/grappa/debugger/web/SparkTesting.java
+++ web/src/main/java/com/github/fge/grappa/debugger/web/SparkTesting.java
@@ -2,15 +2,34 @@ package com.github.fge.grappa.debugger.web;
 
 import spark.Spark;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public final class SparkTesting
 {
     public static void main(final String... args)
     {
-        Spark.staticFileLocation("/static");
+        Spark.staticFileLocation("/site");
 
         Spark.port(8080);
 
-        Spark.get("/", (request, response) -> "hello");
+        Spark.post("/upload", (request, response) -> {
+            final Path tempFile = Files.createTempFile("foo", ".bar");
+            try (
+                final InputStream in = request.raw().getInputStream();
+                final OutputStream out = Files.newOutputStream(tempFile);
+            ) {
+                final byte[] buf = new byte[16384];
 
+                int nrBytes;
+                while ((nrBytes = in.read(buf)) != -1)
+                    out.write(buf, 0, nrBytes);
+            }
+            System.out.println("file saved as " + tempFile);
+            response.redirect("/");
+            return "OK";
+        });
     }
 }
