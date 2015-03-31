90ca6f46bc08174d9dbc0c6426f8d4cb1b8c0b59
Francis Galiegue <fgaliegue@gmail.com>
2015-03-31 19:21:25 +0200

    Make file upload example work, at last
    Not with Jetty proper however; it's quite surprising that this is not handled by
    default...

--- web/src/main/java/com/github/fge/grappa/debugger/web/SparkTesting.java
+++ web/src/main/java/com/github/fge/grappa/debugger/web/SparkTesting.java
@@ -1,9 +1,18 @@
 package com.github.fge.grappa.debugger.web;
 
+import com.oreilly.servlet.multipart.FilePart;
+import com.oreilly.servlet.multipart.MultipartParser;
+import com.oreilly.servlet.multipart.Part;
 import spark.Spark;
+
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public final class SparkTesting
 {
+
     public static void main(final String... args)
     {
         Spark.staticFileLocation("/site");
@@ -11,7 +20,19 @@ public final class SparkTesting
         Spark.port(8080);
 
         Spark.post("/upload", (request, response) -> {
-            System.out.println(request.queryParams());
+            final MultipartParser parser
+                = new MultipartParser(request.raw(), Integer.MAX_VALUE);
+
+            final Part part = parser.readNextPart();
+
+            final Path path = Paths.get("/tmp/meh");
+
+            try (
+                final OutputStream out = Files.newOutputStream(path);
+            ) {
+                ((FilePart) part).writeTo(out);
+            }
+
             response.redirect("/");
             return "OK";
         });
