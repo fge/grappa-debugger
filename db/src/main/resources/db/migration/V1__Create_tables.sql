359b2509dfd4a99f1d4c4f3f936024e68863a3b2
Francis Galiegue <fgaliegue@gmail.com>
2015-04-09 18:04:58 +0200

    Add db module; update h2 dependency

---
+++ db/src/main/resources/db/migration/V1__Create_tables.sql
@@ -0,0 +1,24 @@
+create table matchers (
+    id integer not null,
+    class_name varchar(255) not null,
+    matcher_type varchar(30) not null,
+    name varchar(1024) not null
+);
+
+alter table matchers add primary key(id);
+
+create table nodes (
+    id integer not null,
+    parent_id integer not null,
+    level integer not null,
+    success integer not null,
+    matcher_id integer not null,
+    start_index integer not null,
+    end_index integer not null,
+    time long not null
+);
+
+alter table nodes add primary key(id);
+alter table nodes add foreign key (matcher_id) references matchers(id);
+create index nodes_parent_id on nodes(parent_id);
+create index nodes_indices on nodes(start_index, end_index);
