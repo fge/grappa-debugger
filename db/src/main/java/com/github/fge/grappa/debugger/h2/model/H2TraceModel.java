470272be8d6ab6486880c6e8d9d0e71b867fedda
Francis Galiegue <fgaliegue@gmail.com>
2015-04-13 15:07:50 +0200

    Finish the h2 model

--- db/src/main/java/com/github/fge/grappa/debugger/h2/model/H2TraceModel.java
+++ db/src/main/java/com/github/fge/grappa/debugger/h2/model/H2TraceModel.java
@@ -1,5 +1,6 @@
 package com.github.fge.grappa.debugger.h2.model;
 
+import com.github.fge.grappa.buffers.InputBuffer;
 import com.github.fge.grappa.debugger.h2.jooq.tables.Nodes;
 import com.github.fge.grappa.debugger.model.TraceModel;
 import com.github.fge.grappa.debugger.model.TraceModelException;
@@ -7,6 +8,8 @@ import com.github.fge.grappa.debugger.model.matches.MatchesData;
 import com.github.fge.grappa.debugger.model.rules.PerClassStatistics;
 import com.github.fge.grappa.debugger.model.tree.ParseTreeNode;
 import com.github.fge.grappa.matchers.MatcherType;
+import com.github.fge.grappa.support.IndexRange;
+import org.jooq.CaseConditionStep;
 import org.jooq.Condition;
 import org.jooq.DSLContext;
 import org.jooq.Field;
@@ -14,20 +17,33 @@ import org.jooq.impl.DSL;
 
 import javax.annotation.Nonnull;
 import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static com.github.fge.grappa.debugger.h2.jooq.Tables.MATCHERS;
 import static com.github.fge.grappa.debugger.h2.jooq.Tables.NODES;
 
+@SuppressWarnings({ "AutoUnboxing", "AutoBoxing" })
 public final class H2TraceModel
     implements TraceModel
 {
-    private final DSLContext jooq;
+    private static final Condition EMPTY_MATCHES_CONDITION = NODES.SUCCESS.eq(1)
+        .and(NODES.START_INDEX.equal(NODES.END_INDEX));
+    private static final Condition NONEMPTY_MATCHES_CONDITION
+        = NODES.SUCCESS.eq(1).and(NODES.START_INDEX.ne(NODES.END_INDEX));
+    private static final Condition FAILED_MATCHES_CONDITION
+        = NODES.SUCCESS.eq(0);
 
-    public H2TraceModel(final DSLContext jooq)
+    private final DSLContext jooq;
+    private final InputBuffer inputBuffer;
+
+    public H2TraceModel(final DSLContext jooq, final InputBuffer inputBuffer)
     {
         this.jooq = jooq;
+        this.inputBuffer = inputBuffer;
     }
 
     @Nonnull
@@ -102,15 +118,73 @@ public final class H2TraceModel
     public Map<Integer, Integer> getDepthMap(final int startLine,
         final int wantedLines)
     {
-        // TODO
-        return null;
+        final List<IndexRange> ranges
+            = IntStream.range(startLine, startLine + wantedLines)
+            .mapToObj(inputBuffer::getLineRange)
+            .collect(Collectors.toList());
+
+        final int startIndex = ranges.get(0).start;
+        final int endIndex = ranges.get(ranges.size() - 1).end;
+        final Condition indexCondition = NODES.START_INDEX.lt(endIndex)
+            .and(NODES.END_INDEX.ge(startIndex));
+
+        final Field<Integer> lineField = getLineField(startLine, ranges);
+
+        final Map<Integer, Integer> ret = new HashMap<>();
+
+        jooq.select(lineField, DSL.max(NODES.LEVEL))
+            .from(NODES)
+            .where(indexCondition)
+            .groupBy(lineField)
+            .forEach(r -> ret.put(r.value1(), r.value2() + 1));
+
+        IntStream.range(startLine, startLine + wantedLines)
+            .forEach(line -> ret.putIfAbsent(line, 0));
+
+        return ret;
     }
 
     @Nonnull
     @Override
     public MatchesData getMatchesData()
     {
-        // TODO
-        return null;
+        final Field<Integer> emptyMatches = DSL.decode()
+            .when(EMPTY_MATCHES_CONDITION, 1).otherwise(0);
+        final Field<Integer> nonEmptyMatches = DSL.decode()
+            .when(NONEMPTY_MATCHES_CONDITION, 1).otherwise(0);
+        final Field<Integer> failedMatches = DSL.decode()
+            .when(FAILED_MATCHES_CONDITION, 1).otherwise(0);
+
+        return jooq.select(MATCHERS.NAME, MATCHERS.MATCHER_TYPE,
+            MATCHERS.CLASS_NAME,
+            DSL.sum(emptyMatches).as("emptyMatches"),
+            DSL.sum(nonEmptyMatches).as("nonEmptyMatches"),
+            DSL.sum(failedMatches).as("failedMatches"))
+            .from(MATCHERS, NODES)
+            .where(MATCHERS.ID.eq(NODES.MATCHER_ID))
+            .groupBy(MATCHERS.NAME, MATCHERS.MATCHER_TYPE, MATCHERS.CLASS_NAME)
+            .fetch().stream()
+            .map(MatchStatisticsMapper.INSTANCE::map)
+            .collect(MatchesData.asCollector());
+    }
+
+    private Field<Integer> getLineField(final int startLine,
+        final List<IndexRange> ranges)
+    {
+        CaseConditionStep<Integer> step = DSL.decode()
+            .when(activeThisRange(ranges.get(0)), startLine);
+
+        final int size = ranges.size();
+
+        for (int i = 1; i < size; i++)
+            step = step.when(activeThisRange(ranges.get(i)), startLine + i);
+
+        return step.as("line");
+    }
+
+    private static Condition activeThisRange(final IndexRange range)
+    {
+        return NODES.START_INDEX.lt(range.end)
+            .and(NODES.END_INDEX.ge(range.start));
     }
 }
---
+++ db/src/main/java/com/github/fge/grappa/debugger/h2/model/RuleInfoMapper.java
@@ -0,0 +1,34 @@
+package com.github.fge.grappa.debugger.h2.model;
+
+import com.github.fge.grappa.debugger.model.tree.RuleInfo;
+import com.github.fge.grappa.matchers.MatcherType;
+import org.jooq.Converter;
+import org.jooq.Record;
+import org.jooq.RecordMapper;
+import org.jooq.impl.EnumConverter;
+
+import static com.github.fge.grappa.debugger.h2.jooq.Tables.MATCHERS;
+
+public final class RuleInfoMapper
+    implements RecordMapper<Record, RuleInfo>
+{
+    public static final RecordMapper<Record, RuleInfo> INSTANCE
+        = new RuleInfoMapper();
+
+    private static final Converter<String, MatcherType> CONVERTER
+        = new EnumConverter<>(String.class, MatcherType.class);
+
+    private RuleInfoMapper()
+    {
+    }
+
+    @Override
+    public RuleInfo map(final Record record)
+    {
+        return new RuleInfo(
+            record.getValue(MATCHERS.CLASS_NAME),
+            record.getValue(MATCHERS.MATCHER_TYPE, CONVERTER),
+            record.getValue(MATCHERS.NAME)
+        );
+    }
+}
