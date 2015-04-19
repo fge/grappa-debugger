9decaa388ab36fb183f6916bcb4367476cc2689b
Francis Galiegue <fgaliegue@gmail.com>
2015-04-19 15:34:04 +0200

    Remove all core code

--- core/src/main/java/com/github/fge/grappa/debugger/csvtrace/tabs/TabView.java
+++
@@ -1,8 +0,0 @@
-package com.github.fge.grappa.debugger.csvtrace.tabs;
-
-import com.github.fge.grappa.debugger.model.common.ParseInfo;
-
-public interface TabView
-{
-    void displayInfo(ParseInfo info);
-}
